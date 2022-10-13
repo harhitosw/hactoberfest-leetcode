7d089a3db42c9c3957dbbe0b46a9b97e9a18edba
Swapnil0803 <109861486+Swapnil0803@users.noreply.github.com>
2022-10-13 15:35:36 +0530

    Create BinaryTree_Inorder_Travsersal.cpp

---
+++ BinaryTree_Inorder_Travsersal.cpp
@@ -0,0 +1,34 @@
+// Binary tree inorder traversal solution for leetcode problem #94
+//Link: https://leetcode.com/problems/binary-tree-inorder-traversal/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s;
+        vector<int> res;
+        do{
+            while(root!=NULL){
+                s.push(root);
+                root=root->left;
+            }
+            if(!s.empty()){
+                root=s.top();
+                s.pop();
+                res.push_back(root->val);
+                root=root->right;       
+            }
+        }while(root!=NULL||!s.empty());
+        return res;
+    }
+};
+    
